c7862f3f8c19009a133b27f52ef083c24a3f298e
Daniel <gormandiver@gmail.com>
2017-03-24 20:09:32 +0000

    undate srtsort made word list

--- srtsort.py
+++ srtsort.py
@@ -1,7 +1,23 @@
-with open('JNS.srt') as jns:
-    alllets = []
+import re
+with open('JNS.srt') as jns, open('all_lets', 'w') as alts, open('keeplets') as klts:
+    count = 0
+    keeplets = []
+    uwords = []
+    for let in klts:
+        keeplets.append(let[0])
+    #print(keeplets)
     for line in jns:
-        for let in list(line):
-            if let not in alllets:
-                alllets.append(let)
-    print(alllets)
+        wordlist = re.split(" ", line)
+        #print(wordlist)
+        for word in wordlist:
+            cleanword = []
+            letlist = list(word)
+            for let in letlist:
+                if let in keeplets:
+                    cleanword.append(let)
+            clnwrd = "".join(cleanword)
+            if clnwrd not in uwords:
+                uwords.append(clnwrd)
+                count += 1
+                print(clnwrd)
+    print(count)
